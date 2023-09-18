493d3a0710a5913943452900f1dd306a1549150e
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-09-18 15:13:50 -0500

    Add functions for getting forecast

--- src/apiFunctions.js
+++ src/apiFunctions.js
@@ -1,4 +1,4 @@
-// Get forecast
+// Get API data
 async function fetchForecast(location) {
   const url =
     'https://api.weatherapi.com/v1/forecast.json?key=f5842f73c66340b1b1a172836231709&days=3&q=';
@@ -30,11 +30,93 @@ async function getCurrentWeather(location) {
     fahrenheit,
     celsius,
   };
-  return currentWeather;
+  console.log(currentWeather);
+}
+
+// Get forecast
+async function getForecastData(location) {
+  const data = await fetchForecast(location);
+  const forecastData = data.forecast;
+  return forecastData;
+}
+
+async function getForecast0(location) {
+  const forecast = await getForecastData(location);
+  const thisDay = forecast.forecastday[0].day;
+  const rainChance = thisDay.daily_chance_of_rain;
+  const fahrenheit = {
+    average: thisDay.avgtemp_f,
+    high: thisDay.maxtemp_f,
+    low: thisDay.mintemp_f,
+  };
+  const celsius = {
+    average: thisDay.avgtemp_c,
+    high: thisDay.maxtemp_c,
+    low: thisDay.mintemp_c,
+  };
+  const forecastDay0 = {
+    rainChance,
+    fahrenheit,
+    celsius,
+  };
+  return forecastDay0;
+}
+
+async function getForecast1(location) {
+  const forecast = await getForecastData(location);
+  const thisDay = forecast.forecastday[1].day;
+  const rainChance = thisDay.daily_chance_of_rain;
+  const fahrenheit = {
+    average: thisDay.avgtemp_f,
+    high: thisDay.maxtemp_f,
+    low: thisDay.mintemp_f,
+  };
+  const celsius = {
+    average: thisDay.avgtemp_c,
+    high: thisDay.maxtemp_c,
+    low: thisDay.mintemp_c,
+  };
+  const forecastDay1 = {
+    rainChance,
+    fahrenheit,
+    celsius,
+  };
+  return forecastDay1;
+}
+
+async function getForecast2(location) {
+  const forecast = await getForecastData(location);
+  const thisDay = forecast.forecastday[2].day;
+  const rainChance = thisDay.daily_chance_of_rain;
+  const fahrenheit = {
+    average: thisDay.avgtemp_f,
+    high: thisDay.maxtemp_f,
+    low: thisDay.mintemp_f,
+  };
+  const celsius = {
+    average: thisDay.avgtemp_c,
+    high: thisDay.maxtemp_c,
+    low: thisDay.mintemp_c,
+  };
+  const forecastDay2 = {
+    rainChance,
+    fahrenheit,
+    celsius,
+  };
+  return forecastDay2;
+}
+
+async function getForecastsWeather(location) {
+  const day0 = await getForecast0(location);
+  const day1 = await getForecast1(location);
+  const day2 = await getForecast2(location);
+
+  const forecast = await Promise.all([day0, day1, day2]);
+  console.log(forecast);
 }
 
 // Get location
 
 // Get astronomy
 
-export { getCurrentWeather };
+export { getCurrentWeather, getForecastsWeather };
