f67b1165173d272419b598792c66b3f7c2d83bfd
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-09-18 17:41:51 -0500

    Add createCite()

--- src/domFunctions.js
+++ src/domFunctions.js
@@ -11,4 +11,32 @@ function createForm() {
   body.appendChild(search);
 }
 
-export { createForm };
+function createCite() {
+  const body = document.querySelector('body');
+
+  const cite = document.createElement('div');
+  cite.setAttribute('id', 'cite');
+  const spanOne = document.createElement('span');
+  spanOne.textContent = 'Photo by ';
+  const citeLink = document.createElement('a');
+  citeLink.href = 'https://unsplash.com/@danielleone';
+  citeLink.textContent = 'Daniel Leone ';
+  const spanTwo = document.createElement('span');
+  spanTwo.textContent = 'on ';
+  const photoLink = document.createElement('a');
+  photoLink.href = 'https://unsplash.com/photos/v7daTKlZzaw';
+  photoLink.textContent = 'Unsplash.';
+
+  cite.appendChild(spanOne);
+  cite.appendChild(citeLink);
+  cite.appendChild(spanTwo);
+  cite.appendChild(photoLink);
+  body.appendChild(cite);
+}
+
+function loadPage() {
+  createForm();
+  createCite();
+}
+
+export { loadPage };
