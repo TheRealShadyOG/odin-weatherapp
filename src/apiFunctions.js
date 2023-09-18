ba1ba561cc51d6f28fff3bcf21f2a7a3bdfe1a56
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-09-18 14:35:36 -0500

    Add getCurrentWeather() and export

--- src/apiFunctions.js
+++ src/apiFunctions.js
@@ -11,7 +11,30 @@ async function fetchForecast(location) {
 }
 
 // Get current weather
+async function getCurrentWeather(location) {
+  const data = await fetchForecast(location);
+  const currentData = data.current;
+  const condition = currentData.condition.text;
+  const humidity = currentData.humidity;
+  const fahrenheit = {
+    feelsLike: currentData.feelslike_f,
+    temp: currentData.temp_f,
+  };
+  const celsius = {
+    feelsLike: currentData.feelslike_c,
+    temp: currentData.temp_c,
+  };
+  const currentWeather = {
+    condition,
+    humidity,
+    fahrenheit,
+    celsius,
+  };
+  return currentWeather;
+}
 
 // Get location
 
 // Get astronomy
+
+export { getCurrentWeather };
