51b27c4a49426cb964345c224c9052bbb0ad614c
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-09-19 11:58:01 -0500

    Add functions to display daily forecast

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ function updateLocation() {
 // Display data about location
 async function displayLocation(location) {
   const locationData = await getLocationData(location);
-  const localTimeData = locationData.localTime.slice(6);
+  const localTimeData = locationData.localTime.slice(5);
   const time = localTimeData.split('-').join('/');
 
   const city = document.querySelector('#city');
@@ -36,9 +36,71 @@ async function displayLocation(location) {
   localTime.textContent = time;
 }
 
+// Display daily forecast data
+async function displayDailyForecast(location, unit) {
+  const forecastData = await getForecastsWeather(location);
+  const dailyData = forecastData[0];
+  const containers = document.querySelectorAll('#forecastday');
+
+  containers.forEach((ele, i) => {
+    // Add each day
+    const day = dailyData[i].date.slice(5);
+    const modifiedDay = day.split('-').join('/');
+    const date = ele.children[0];
+    date.textContent = modifiedDay;
+    // Add each condition
+    const dayCondition = dailyData[i].condition;
+    const condition = ele.children[1];
+    condition.textContent = dayCondition;
+    // Add each temp
+    const tempDiv = ele.children[2];
+    if (unit === 'F') {
+      getDailyFahrenheit(tempDiv, dailyData[i]);
+    } else {
+      getDailyCelsius(tempDiv, dailyData[i]);
+    }
+  });
+}
+
+// Display Daily forecast in fahrenheit
+function getDailyFahrenheit(div, data) {
+  const temps = data.fahrenheit;
+  const high = div.children[0];
+  const highTemp = 'High ' + temps.high;
+  high.textContent = highTemp;
+  const low = div.children[1];
+  const lowTemp = 'Low ' + temps.low;
+  low.textContent = lowTemp;
+  const avg = div.children[2];
+  const avgTemp = temps.average;
+  avg.textContent = 'Avg ' + avgTemp;
+}
+
+// Display Daily forecast in celsius
+function getDailyCelsius(div, data) {
+  const temps = data.celsius;
+  const high = div.children[0];
+  const highTemp = 'High ' + temps.high;
+  high.textContent = highTemp;
+  const low = div.children[1];
+  const lowTemp = 'Low ' + temps.low;
+  low.textContent = lowTemp;
+  const avg = div.children[2];
+  const avgTemp = temps.average;
+  avg.textContent = 'Avg ' + avgTemp;
+}
+
 displayLocation(location);
+displayDailyForecast(location, 'F');
 
 // Temporary function calls
 getCurrentWeather(location);
-getForecastsWeather(location);
 getAstronomyData(location);
+
+// console.log(dailyData);
+// dailyData.forEach((ele) => {
+//   const condition = document.querySelector('#dayforecastcondition');
+//   // condition.textContent = ;
+//   const temp = document.querySelector('#dayforecasttemp');
+//   console.log(modifiedDay);
+// });
